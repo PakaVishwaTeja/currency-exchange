1e49310d0af0aa45c091ef1671de70a4d6dbe627
vishwa <tejapaka08@gmail.com>
2023-08-21 11:19:57 +0530

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { CustomerHomeComponent } from './components/customer-home/customer-home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
+import { AdminHomeComponent } from './components/admin-home/admin-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path : 'signin' , component : SigninComponent},
+   {path : 'admin' , component : AdminHomeComponent},
+   {path : 'customer' , component : CustomerHomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { AdminHomeComponent } from './components/admin-home/admin-home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CustomerHomeComponent } from './components/customer-home/customer-home.component';
+import { CustomerPlaceOrderComponent } from './components/customer-place-order/customer-place-order.component';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    AdminHomeComponent,
+    HeaderComponent,
+    CustomerHomeComponent,
+    CustomerPlaceOrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
